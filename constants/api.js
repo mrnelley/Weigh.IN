dfae7c98efeaf1fd8ef5e0e5072c0ee3e7a768c2
Parnell Kelley <Nelley@Nelley.local>
2018-03-19 20:36:03 -0700

    added API routes and started front end querying

--- constants/api.js
+++ constants/api.js
@@ -1,3 +1,25 @@
-export const fetchUsers = () =>
-  fetch("http://localhost:3000/api/users")
-    .then(res => res.json());
+// export const fetchUsers = () =>
+//   fetch("http://localhost:3000/api/users")
+//     .then(res => res.json());
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3000/api';
+
+const fakeGroupId = '5aa70f47c58cba576a1fb0b';
+
+class EventApi {
+  constructor() {
+    this.groupId = fakeGroupId;
+    this.path = '/groups/${this.groupId}/events';
+  }
+
+  async fetchGroupsEvents() {
+    const { data } = await axios.get(this.path);
+
+    return data.events;
+  }
+}
+
+export {
+  EventApi,
+};
---
+++ constants/colors.js
@@ -0,0 +1,5 @@
+export default {
+  blackcolor: 'black',
+  whitecolor: '#fff',
+
+};
